6011297bab3a862d986a7f804fe5b684cca35c58
Mona <monabadee@gmail.com>
2023-11-22 21:08:33 +0300

    Complete  Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    // Define the data object
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    // Define the fetch request with the required parameters
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Access the id from the response and append it to the DOM
+        document.body.innerHTML += data.id;
+      })
+      .catch(error => {
+        // Append the error message to the DOM
+        document.body.innerHTML += error.message;
+      });
+  }
+  
+  // Return the fetch chain
+module.exports = submitData;
+
+
+  submitData("Muna", "monabadee@gmail.com");
